77c30c35bc529b8ee11186c59ba7aa6f959ab8b8
Sebastian <139996363+derSebbl@users.noreply.github.com>
2025-03-10 14:39:33 +0100

    .

--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3018e435f1a5ef07281f1a15c253005702b8b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5644d3264ca3c5e07168efa6bba5ae34ddc9800")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
